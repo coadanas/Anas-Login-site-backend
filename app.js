86349b1d058a3edfc042e657575aba4e1cb7353b
coadanas <147676534+coadanas@users.noreply.github.com>
2024-05-02 18:04:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const JWT = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// CORS CROS
+const cors = require("cors");
+var corsOptions = {
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const port = 5000;
+require("dotenv").config();
+app.use(bodyParser.json());
+
+// USERMODEL && MONGOOSE CONNECTION
+const User = require("./userModel/userModel.js");
+
+// REGISTER ROUTE
+const registerRouter = require("./routers/Register.js");
+app.use("/auth", registerRouter);
+
+// LOGIN ROUTE
+const loginRouter = require("./routers/login.js");
+app.use("/auth", loginRouter);
+
+// USER ROUTE
+const userRouter = require("./routers/user.js");
+app.use("/auth", userRouter);
+
+// USER ROUTE
+const contactRouter = require("./routers/contactRouter.js");
+app.use("/", contactRouter);
+
+// SERVER LISTEN
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
